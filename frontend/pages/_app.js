2d965542d703ac33519db151b506d4e54977b158
bevhatzl <beverleyhatzl@gmail.com>
2021-03-20 15:41:20 +1100

    setup Apollo Client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,22 +1,37 @@
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from "../components/Page";
-
 // To display progress bar when loading
 // import 'nprogress/nprogress.css';    
 // Instead of the above line, using own custom styles 
 import '../components/styles/nprogress.css';
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 
-export default function MyApp( {Component, pageProps} ) {
+function MyApp( {Component, pageProps, apollo} ) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-
-    </Page>
+      <ApolloProvider client={apollo}>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ApolloProvider>
   );
 }
+
+// getInitialProps is a nextjs async method. ctx is the context.
+MyApp.getInitialProps = async function({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);    
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
