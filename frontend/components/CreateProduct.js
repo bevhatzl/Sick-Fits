df24f9f323861ecd4025337a9197ff7f46a04300
bevhatzl <beverleyhatzl@gmail.com>
2021-03-24 20:28:37 +1100

    create products using mutations

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,5 +1,39 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
+
+const CREATE_PRODUCT_MUTATION = gql`
+# This is all graphql notation and not javascript
+  mutation CREATE_PRODUCT_MUTATION(
+    # which variables are getting passed in ? ANd what types are they?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+        data:{
+          name: $name
+          description: $description
+          price: $price
+          status: "AVAILABLE"
+          photo: {
+            create: {
+              image: $image,
+              altText: $name
+            }
+          }
+        }
+    ) {
+      id
+      price
+      description
+      name
+	  }
+  }
+`;
 
 export default function CreateProduct() {
     const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -8,13 +42,19 @@ export default function CreateProduct() {
       price: 20399,
       description: 'These are the best shoes.'
     });
-
+    const [createProduct, {loading, error, data}] = useMutation(CREATE_PRODUCT_MUTATION, {
+      variables: inputs
+    });
     return (
-    <Form onSubmit={(e) => {
+    <Form onSubmit={async (e) => {
       e.preventDefault();
       console.log(inputs);
+      // Submit the inputfields to the backend:
+      await createProduct();
+      clearForm();
     }}>
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input required
