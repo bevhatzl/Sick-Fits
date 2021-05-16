305b0ae7ba1551a82ed503a19146c474e3461f75
bevhatzl <beverleyhatzl@gmail.com>
2021-05-16 16:42:29 +1000

    Creating Order and OrderItem Data Types

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -2,6 +2,8 @@ import { CartItem } from './schemas/CartItem';
 import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { OrderItem } from './schemas/OrderItem';
+import { Order } from './schemas/Order';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
@@ -56,7 +58,9 @@ export default withAuth(config({
         User,   // Property name and value are the same. 
         Product,
         ProductImage,
-        CartItem
+        CartItem,
+        OrderItem,
+        Order
     }),
     extendGraphqlSchema: extendGraphqlSchema,
     ui: {
