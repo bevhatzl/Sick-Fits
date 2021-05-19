6b5665babd20ea246cd5d4f49d39ad1a120ee281
bevhatzl <beverleyhatzl@gmail.com>
2021-05-19 22:20:44 +1000

    Permissions access and rule based functions

--- backend/access.ts
+++ backend/access.ts
@@ -1,3 +1,4 @@
+import { permissionsList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 
 // The access control returns a yes or a no value depending if the user is signed in. 
@@ -6,3 +7,40 @@ export function isSignedIn({ session }: ListAccessArgs) {
   // The "!!" means that.. If undefined, it will return false (meaning they are not signed in). If they are signed in it will return true. The !! coerces the truthy and falsy values to booleans of true or false.
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(permissionsList.map
+  (permission => [
+    permission, 
+    function({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission]
+    }
+  ])    
+);
+
+// Permissions check if someone meets a criteria - yes or no.
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs): boolean {
+    return session?.data.name.includes('bev');
+  }
+};
+
+// Rule based functions
+// Rules can return a boolean - yes or no - or a filter which limits which products they can CRUD.
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    // 1. Do they have the permission of canManageProducts?
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    // 2. If not, do they own this item?
+    return { user: { id: session.itemId }};
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (permissions.canManageProducts({ session })) {
+      return true; // They can read everything!
+    }
+    // They should only see available products based on the status field
+    return { status: 'AVAILABLE' }
+  }
+};
